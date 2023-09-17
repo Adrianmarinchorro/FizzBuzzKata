6713868327244e4896898f2e190fd2faa265819e
FuenSansano <fuensanosano@gmail.com>
2023-09-17 13:42:51 +0200

    given_6_then_return_fizz

--- php/src/FizzBuzz.php
+++ php/src/FizzBuzz.php
@@ -7,8 +7,8 @@ class FizzBuzz
     public function convert(int $number): string
     {
         if ($number === 5) return 'Buzz';
-        
-        if ($number === 3) return 'Fizz';
+
+        if ($number === 3 || $number === 6) return 'Fizz';
 
         return (string) $number;
     }
